a041f5c84c727ca05e1b4265e39a170dc77e9c54
Tine MS Petersen <tinemeretesikjaerpetersen@gmail.com>
2025-03-13 14:12:56 +0100

    Connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from 'express'
+import connectDB from './config/mongodb.js';
+import 'dotenv/config'
+
 
 
 // App Config
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
