8bd6f9de61670b1e18acbf320abae8cf1341da69
Tine MS Petersen <tinemeretesikjaerpetersen@gmail.com>
2025-03-23 14:32:01 +0100

    Fix text

--- frontend/src/pages/Project.tsx
+++ frontend/src/pages/Project.tsx
@@ -223,7 +223,8 @@ const Project = () => {
           <div className="flex flex-col gap-5 items-center">
             <p className="text-2xl">Mark complete</p>
             <p>
-              Here you can change the due date to another time in the calendar.
+            This will mark the project as complete, and remove it from the
+            "Projects" tab and the calendar.
             </p>
             <div>
               <p className="mb-2">Is the project complete?</p>
@@ -254,8 +255,7 @@ const Project = () => {
           <div className="flex flex-col gap-5 items-center">
             <p className="text-2xl">Change Due Date</p>
             <p>
-              This will mark the project as complete, and remove it from the
-              "Projects" tab and the calendar.
+             Here you can change the due date to another time in the calendar.
             </p>
             <div>
               <form
